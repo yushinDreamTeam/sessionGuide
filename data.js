0bf4b0a5a32d843a070fd20e69abef6b4f20d347
jhofjos18 <131941251+jhofjos18@users.noreply.github.com>
2026-07-14 02:09:27 +0900

    Update data.js

--- data.js
+++ data.js
@@ -14,7 +14,6 @@ const FACULTIES = ["공학", "자연", "인문", "사회", "의약", "교육", "
 
 const UNIVERSITIES = {
   "건국대": { logo: "assets/logos/건국대.png" },
-  "경북대": { logo: "assets/logos/경북대.png" },
   "경희대": { logo: "assets/logos/경희대.png" },
   "고려대": { logo: "assets/logos/고려대.png" },
   "국민대": { logo: "assets/logos/국민대.png" },
@@ -23,7 +22,6 @@ const UNIVERSITIES = {
   "서울시립대": { logo: "assets/logos/서울시립대.png" },
   "숭실대": { logo: "assets/logos/숭실대.png" },
   "중앙대": { logo: "assets/logos/중앙대.png" },
-  "한양대(ERICA)": { logo: "assets/logos/한양대(ERICA).png" },
 };
 
 const SUBJECTS = {
